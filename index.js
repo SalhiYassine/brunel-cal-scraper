7cfc82a7f43ab012bbd6ab3e2b697e0fd0befa14
Yassine Salhi <yassinesalhi135@gmail.com>
2021-09-23 22:29:20 +0100

    fixed it xD

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const puppeteer = require("puppeteer");
+const ical = require('ical-generator')
+
+
 
 let formated_results = (monday_date, result) =>{
-  function addDays(date, days) {
-      var result = new Date(date);
-      result.setDate(result.getDate() + days);
-      return result;
+    function addDays(date, days) {
+        var result = new Date(date);
+        result.setDate(result.getDate() + days-1);
+        return result;
     }
     let events = []
     result.map((day)=>{
@@ -12,13 +15,16 @@ let formated_results = (monday_date, result) =>{
         day.map((event, index)=>{
             if(index !== 0){
                 let temp_desc = event[1].replace(/\s/g, '');
+                const date = addDays(monday_date, event[8])
                 let e = {
-                    title: temp_desc ? event[1] : event[0],
-                    start: event[2],
-                    end: event[3],
+                    summary: temp_desc ? event[1] : event[0],
+                    start: new Date(date.getFullYear(), date.getMonth(), date.getDate(), event[2].split(':')[0], event[2].split(':')[1], 0 ),
+                    end: new Date(date.getFullYear(), date.getMonth(), date.getDate(), event[3].split(':')[0], event[3].split(':')[1], 0 ),
                     location: event[5],
                     date: addDays(monday_date, event[8])
                 }
+                console.log(e)
+                
                 
                 events.push(e)
             }
@@ -32,7 +38,7 @@ let formated_results = (monday_date, result) =>{
 }
 
 const getWeekData = async (page) =>{
-
+    
     const monday_date = await page.evaluate(() => {
         function getMonthFromString(mon){
             return new Date(Date.parse(mon +" 1, 2012")).getMonth()+1
@@ -73,9 +79,10 @@ const getWeekData = async (page) =>{
     page.setDefaultNavigationTimeout(0);
     await page.goto('https://teaching.brunel.ac.uk/teaching/SWS-2122/login.aspx');
     
-    await page.type('[name=tUserName]', 'someone');
-    await page.type('[name="tPassword"]', 'password');
+    await page.type('[name=tUserName]', '1904620');
+    await page.type('[name="tPassword"]', 'Rocket52');
     await page.click('[type=submit]');
+    console.log("User Logged In")
     await page.waitForTimeout(400)
     await page.click('a[id="LinkBtn_mystudentsettimetable"]')
     await page.waitForTimeout(400)
@@ -87,6 +94,7 @@ const getWeekData = async (page) =>{
     await page.waitForTimeout(400)
     let year = []
     for(let i = 0; i<52; i++){
+        console.log("Fetched Week: "+i)
         await page.waitForTimeout(400)
         let week = await getWeekData(page)
         if(week.length > 0){
@@ -98,10 +106,14 @@ const getWeekData = async (page) =>{
     }
     console.log( year)
     let numLec = 0
-    year.forEach(week=> week.forEach(day=> numLec++))
+    let cal = ical({domain: 'brunel.ac.uk', name: 'University Timetable'});
+    year.forEach(week=> week.forEach(event=> {
+        numLec++;
+        cal.createEvent({...event})
+    }))
+    cal.saveSync('./callendar.ical')
     console.log("You have " + year.length+" weeks of lectures & labs: " + numLec + " in total")
-
-
+    
     // console.log(await page.content());
     
     await browser.close();
