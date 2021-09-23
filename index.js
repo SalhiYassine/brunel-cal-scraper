3ed64f28b737defdb32161527ef3139b2676e2f0
Yassine Salhi <yassinesalhi135@gmail.com>
2021-09-23 11:08:09 +0100

    working concept

--- index.js
+++ index.js
@@ -1,37 +1,49 @@
 const puppeteer = require("puppeteer");
 
+let formated_results = (monday_date, result) =>{
+  function addDays(date, days) {
+      var result = new Date(date);
+      result.setDate(result.getDate() + days);
+      return result;
+    }
+    let events = []
+    result.map((day)=>{
+        
+        day.map((event, index)=>{
+            if(index !== 0){
+                let temp_desc = event[1].replace(/\s/g, '');
+                let e = {
+                    title: temp_desc ? event[1] : event[0],
+                    start: event[2],
+                    end: event[3],
+                    location: event[5],
+                    date: addDays(monday_date, event[8])
+                }
+                
+                events.push(e)
+            }
+        })
+    })
+    
+    
+    
+    
+    return events
+}
 
-(async ()=> {
+const getWeekData = async (page) =>{
 
-    
-    const browser = await puppeteer.launch();
-    
-    const page = await browser.newPage();
-    await page.goto('https://teaching.brunel.ac.uk/teaching/SWS-2122/login.aspx');
-    
-    await page.type('[name=tUserName]', '1908906');
-    await page.type('[name="tPassword"]', 'Katia1974');
-    await page.click('[type=submit]');
-    await page.waitForTimeout(400)
-    await page.click('a[id="LinkBtn_mystudentsettimetable"]')
-    await page.waitForTimeout(400)
-    await page.select('select[name="lbWeeks"]','1')
-    await page.waitForTimeout(400)
-    await page.select('select[name="dlType"]','TextSpreadsheet;swsurl;SWSCUST Object TextSpreadsheet')
-    await page.waitForTimeout(400)
-    await page.click('[type=submit]');
-    await page.waitForTimeout(400)    
     const monday_date = await page.evaluate(() => {
         function getMonthFromString(mon){
             return new Date(Date.parse(mon +" 1, 2012")).getMonth()+1
-         }
+        }
         let monday_date = document.getElementsByClassName('header-1-2-3')[0].textContent.split('-')[0].split(" ")
         let day = monday_date[0]
         let month = getMonthFromString(monday_date[1])
         let year = monday_date[2]
-        return new Date(`${year}-${month}-${day}`)
-      });
-    console.log(monday_date)
+        return `${year}-${month}-${day}`
+    });
+    
     const result = await page.$$eval('.spreadsheet', tables => {
         let week = []
         for(let i = 0; i < tables.length; i++){
@@ -47,36 +59,46 @@ const puppeteer = require("puppeteer");
             week.push(day)
         }
         return week
-      });
+    });
+    return formated_results(monday_date, result);
+}
+
+
+(async ()=> {
     
-      let formated_results = (monday_date, result) =>{
-          let events = []
-          result.map((day)=>{
-             
-              day.map((event, index)=>{
-                  if(index !== 0){
+    
+    const browser = await puppeteer.launch();
+    
+    const page = await browser.newPage();
+    page.setDefaultNavigationTimeout(0);
+    await page.goto('https://teaching.brunel.ac.uk/teaching/SWS-2122/login.aspx');
+    
+    await page.type('[name=tUserName]', '1908906');
+    await page.type('[name="tPassword"]', 'Katia1974');
+    await page.click('[type=submit]');
+    await page.waitForTimeout(400)
+    await page.click('a[id="LinkBtn_mystudentsettimetable"]')
+    await page.waitForTimeout(400)
+    await page.select('select[name="lbWeeks"]','1')
+    await page.waitForTimeout(400)
+    await page.select('select[name="dlType"]','TextSpreadsheet;swsurl;SWSCUST Object TextSpreadsheet')
+    await page.waitForTimeout(400)
+    await page.click('[type=submit]');
+    await page.waitForTimeout(400)
+    let year = []
+    for(let i = 0; i<52; i++){
+        await page.waitForTimeout(400)
+        let week = await getWeekData(page)
+        if(week.length > 0){
+            year.push(week)
+        }
+        await page.click('a[id="bNextWeek"]')
+        await page.waitForTimeout(400)
 
-                      let e = {
-                          title: event[0],
-                          description: event[1],
-                          start: event[2],
-                          end: event[3],
-                          room: event[4]
-                        }
-                        console.log(e)
-                        events.push(e)
-                    }
-                })
-            })
-            
-            
-            
-
-        return events
     }
-    console.log(formated_results(monday_date, result))
+    console.log( year)
+
     // console.log(await page.content());
-    await page.screenshot({path: 'screenshot.png'});
     
     await browser.close();
 })();
